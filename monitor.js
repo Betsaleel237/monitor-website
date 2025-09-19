e9e3cfe0cae836c4dea67ff64cde5fe8f8f2db20
Betsaleel237 <140754338+Betsaleel237@users.noreply.github.com>
2025-09-19 10:14:56 +0100

    Update2 monitor.js

--- monitor.js
+++ monitor.js
@@ -6,8 +6,14 @@ const puppeteer = require("puppeteer");
   console.log(`🔍 Checking site: ${url}`);
 
   const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: "true",
+    args: [
+      "--no-sandbox", 
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--disable-software-rasterizer",
+    ],
   });
   const page = await browser.newPage();
 
